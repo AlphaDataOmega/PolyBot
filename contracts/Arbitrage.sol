804625403fc23d6c09860b6297b12277a636c6a3
AlphaDataOmega <admin@alphadataomega.com>
2022-05-13 17:04:04 -0500

    Working on dynamic FlashLoan

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -1,251 +1,182 @@
 // SPDX-License-Identifier: MIT
-pragma solidity <=0.8.10;
+pragma solidity <=0.8.13;
 
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface Structs { //set up a struct called Val with a unsigned interger variable inside called value.
-    struct Val {
-        uint256 value;
-    }
+//Here we will set up a new interface for DODO which includes a function called flashloan()
+interface IDODO {
+    /*
+    *@Developer: flashloan()
+    *@Params: baseAmount, quoteAmount, assetTo, data
+    *
+    * */
+    function flashLoan(
+        uint256 baseAmount,
+        uint256 quoteAmount,
+        address assetTo,
+        bytes calldata data
+    ) external;
 
-    enum ActionType { //enum works like structs but is meant to hold many different options. You can add enums inside of structs
-        Deposit, // supply tokens
-        Withdraw, // borrow tokens
-        Transfer, // transfer balance between accounts
-        Buy, // buy an amount of some token (externally)
-        Sell, // sell an amount of some token (externally)
-        Trade, // trade tokens against another account
-        Liquidate, // liquidate an undercollateralized or expiring account
-        Vaporize, // use excess tokens to zero-out a completely negative account
-        Call // send arbitrary data to an address
-    }
-
-    enum AssetDenomination {
-        Wei // the amount is denominated in wei
-    }
-
-    enum AssetReference {
-        Delta // the amount is given as a delta from the current value
-    }
-
-    struct AssetAmount { // here we see that the enums above are added into this struct.
-        bool sign; // true if positive
-        AssetDenomination denomination;
-        AssetReference ref;
-        uint256 value;
-    }
-
-    struct ActionArgs { //here we see that the enum actionType is added inside of this struct.
-        ActionType actionType;
-        uint256 accountId;
-        AssetAmount amount;
-        uint256 primaryMarketId;
-        uint256 secondaryMarketId;
-        address otherAddress;
-        uint256 otherAccountId;
-        bytes data;
-    }
-
-    struct Info {
-        address owner; // The address that owns the account
-        uint256 number; // A nonce that allows a single address to control many accounts
-    }
-
-    struct Wei {
-        bool sign; // true if positive
-        uint256 value;
-    }
+    function _BASE_TOKEN_() external view returns (address);
 }
 
-abstract contract DyDxPool is Structs {
-    function getAccountWei(Info memory account, uint256 marketId)
-        public
-        view
-        virtual
-        returns (Wei memory);
 
-    function operate(Info[] memory, ActionArgs[] memory) public virtual;
-}
+contract Arbitrage {
 
-contract DyDxFlashLoan is Structs {
-    DyDxPool pool = DyDxPool(0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e);
-
-    address public WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    mapping(address => uint256) public currencies;
-
-    constructor() {
-        currencies[WETH] = 1;
-    }
-
-    modifier onlyPool() {
-        require(
-            msg.sender == address(pool),
-            "FlashLoan: could be called by DyDx pool only"
-        );
-        _;
-    }
-
-    function tokenToMarketId(address token) public view returns (uint256) {
-        uint256 marketId = currencies[token];
-        require(marketId != 0, "FlashLoan: Unsupported token");
-        return marketId - 1;
-    }
-
-    // the DyDx will call `callFunction(address sender, Info memory accountInfo, bytes memory data) public` after during `operate` call
-    function flashloan(
-        address token,
-        uint256 amount,
-        bytes memory data
-    ) internal {
-        IERC20(token).approve(address(pool), amount + 1);
-        Info[] memory infos = new Info[](1);
-        ActionArgs[] memory args = new ActionArgs[](3);
-
-        infos[0] = Info(address(this), 0);
-
-        AssetAmount memory wamt = AssetAmount(
-            false,
-            AssetDenomination.Wei,
-            AssetReference.Delta,
-            amount
-        );
-        ActionArgs memory withdraw;
-        withdraw.actionType = ActionType.Withdraw;
-        withdraw.accountId = 0;
-        withdraw.amount = wamt;
-        withdraw.primaryMarketId = tokenToMarketId(token);
-        withdraw.otherAddress = address(this);
-
-        args[0] = withdraw;
-
-        ActionArgs memory call;
-        call.actionType = ActionType.Call;
-        call.accountId = 0;
-        call.otherAddress = address(this);
-        call.data = data;
-
-        args[1] = call;
-
-        ActionArgs memory deposit;
-        AssetAmount memory damt = AssetAmount(
-            true,
-            AssetDenomination.Wei,
-            AssetReference.Delta,
-            amount + 1
-        );
-        deposit.actionType = ActionType.Deposit;
-        deposit.accountId = 0;
-        deposit.amount = damt;
-        deposit.primaryMarketId = tokenToMarketId(token);
-        deposit.otherAddress = address(this);
-
-        args[2] = deposit;
-
-        pool.operate(infos, args);
-    }
-}
-
-contract Arbitrage is DyDxFlashLoan {
-    IUniswapV2Router02 public immutable sRouter;
-    IUniswapV2Router02 public immutable uRouter;
+    IUniswapV2Router02 public immutable router1;
+    IUniswapV2Router02 public immutable router2;
 
     address public owner;
 
-    constructor(address _sRouter, address _uRouter) {
-        sRouter = IUniswapV2Router02(_sRouter); // Sushiswap
-        uRouter = IUniswapV2Router02(_uRouter); // Uniswap
+    constructor() {
         owner = msg.sender;
     }
 
+    function dodoFlashLoan(
+    address router1,
+    address router2,
+    address flashLoanPool, //You will make a flashloan from this DODOV2 pool
+    address loanToken,
+    uint256 loanAmount,
+    bool startOnRoute1,
+    address token1,
+    uint256 balanceBefore 
+
+    ) internal  {
+        //Note: The data can be structured with any variables required by your logic. The following code is just an example
+        bytes memory data = abi.encode(router1, router2, flashLoanPool, loanToken, loanAmount, startOnRoute1, token1, balanceBefore);
+        address flashLoanBase = IDODO(flashLoanPool)._BASE_TOKEN_();
+        if(flashLoanBase == loanToken) {
+            IDODO(flashLoanPool).flashLoan(loanAmount, 0, address(this), data);
+        } else {
+            IDODO(flashLoanPool).flashLoan(0, loanAmount, address(this), data);
+        }
+    }
+
     function executeTrade(
-        bool _startOnUniswap,
+        address _router1,
+        address _router2,
+        address _flashLoanPool,
+        bool _startOnRoute1,
         address _token0,
         address _token1,
         uint256 _flashAmount
     ) external {
+
+        router1 = IUniswapV2Router02(_router1); 
+        router2 = IUniswapV2Router02(_router2); 
+        
         uint256 balanceBefore = IERC20(_token0).balanceOf(address(this));
 
-        bytes memory data = abi.encode(
-            _startOnUniswap,
-            _token0,
-            _token1,
-            _flashAmount,
-            balanceBefore
-        );
-
-        flashloan(_token0, _flashAmount, data); // execution goes to `callFunction`
+        dodoFlashLoan(_router1, _router2, _flashLoanPool, _token0, _flashAmount, _startOnRoute1, _token1, balanceBefore); // execution should trigger callback function
     }
 
-    function callFunction(
-        address, /* sender */
-        Info calldata, /* accountInfo */
+     function DVMFlashLoanCall(
+        address sender, 
+        uint256 baseAmount, 
+        uint256 quoteAmount,
         bytes calldata data
-    ) external onlyPool {
+        ) external {
+        _flashLoanCallBack(sender,baseAmount,quoteAmount,data);
+    }
+
+    //Note: CallBack function executed by DODOV2(DPP) flashLoan pool
+    function DPPFlashLoanCall(
+        address sender,
+        uint256 baseAmount,
+        uint256 quoteAmount,
+        bytes calldata data
+           ) external {
+        _flashLoanCallBack(sender,baseAmount,quoteAmount,data);
+    }
+
+    //Note: CallBack function executed by DODOV2(DSP) flashLoan pool
+    function DSPFlashLoanCall(
+        address sender, 
+        uint256 baseAmount, 
+        uint256 quoteAmount,
+        bytes calldata data
+         ) external {
+        _flashLoanCallBack(sender,baseAmount,quoteAmount,data);
+    }
+
+
+    function _flashLoanCallBack(
+        address sender, 
+        uint256, 
+        uint256,
+        bytes calldata data
+        ) internal {
         (
-            bool startOnUniswap,
+            address flashLoanPool,
             address token0,
-            address token1,
             uint256 flashAmount,
+            address route1,
+            address route2,
+            bool startOnRoute1,
+            address token1,
             uint256 balanceBefore
-        ) = abi.decode(data, (bool, address, address, uint256, uint256));
+        ) = abi.decode(data, (address, address, uint256, address, address, bool, address, uint256));
 
         uint256 balanceAfter = IERC20(token0).balanceOf(address(this));
 
-        require(
-            balanceAfter - balanceBefore == flashAmount,
-            "contract did not get the loan"
-        );
+        require(sender == address(this) && msg.sender == flashLoanPool, "HANDLE_FLASH_DENIED");
+        require(balanceAfter - balanceBefore == flashAmount, "contract did not get the loan");
 
-        // Use the money here!
+        //Use the money here!
+        address router1;
+        address router2;
+
         address[] memory path = new address[](2);
 
         path[0] = token0;
         path[1] = token1;
 
-        if (startOnUniswap) {
-            _swapOnUniswap(path, flashAmount, 0);
+        if (startOnRoute1) {
+            _swapOnRoute1(router1, path, flashAmount, 0);
 
             path[0] = token1;
             path[1] = token0;
 
-            _swapOnSushiswap(
+            _swapOnRoute2(
+                router2,
                 path,
                 IERC20(token1).balanceOf(address(this)),
                 (flashAmount + 1)
             );
         } else {
-            _swapOnSushiswap(path, flashAmount, 0);
+            _swapOnRoute2(router2, path, flashAmount, 0);
 
             path[0] = token1;
             path[1] = token0;
 
-            _swapOnUniswap(
+            _swapOnRoute1(
+                router1,
                 path,
                 IERC20(token1).balanceOf(address(this)),
                 (flashAmount + 1)
             );
         }
 
-        IERC20(token0).transfer(
-            owner,
-            IERC20(token0).balanceOf(address(this)) - (flashAmount + 1)
-        );
+        IERC20(token0).transfer(owner, IERC20(token0).balanceOf(address(this)) - (flashAmount + 1));
+        IERC20(token0).transfer(flashLoanPool, flashAmount);
     }
 
     // -- INTERNAL FUNCTIONS -- //
 
-    function _swapOnUniswap(
+    function _swapOnRoute1(
+        address router1,
         address[] memory _path,
         uint256 _amountIn,
         uint256 _amountOut
     ) internal {
         require(
-            IERC20(_path[0]).approve(address(uRouter), _amountIn),
-            "Uniswap approval failed."
+            IERC20(_path[0]).approve(address(router1), _amountIn),
+            "Exchange 1 approval failed."
         );
 
-        uRouter.swapExactTokensForTokens(
+        router1.swapExactTokensForTokens(
             _amountIn,
             _amountOut,
             _path,
@@ -254,17 +185,18 @@ contract Arbitrage is DyDxFlashLoan {
         );
     }
 
-    function _swapOnSushiswap(
+    function _swapOnRoute2(
+        address router2,
         address[] memory _path,
         uint256 _amountIn,
         uint256 _amountOut
     ) internal {
         require(
-            IERC20(_path[0]).approve(address(sRouter), _amountIn),
-            "Sushiswap approval failed."
+            IERC20(_path[0]).approve(address(router2), _amountIn),
+            "Exchange 2 approval failed."
         );
 
-        sRouter.swapExactTokensForTokens(
+        router2.swapExactTokensForTokens(
             _amountIn,
             _amountOut,
             _path,
